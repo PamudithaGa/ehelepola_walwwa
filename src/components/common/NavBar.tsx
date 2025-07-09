3af7f1cd37c4009798aa2e07ec7eb511083714ed
Ashan Vimukthi <ashanwithanarachchi@gmail.com>
2025-07-09 16:10:15 +0530

    update navbar

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -17,14 +17,16 @@ const NavBar: React.FC<NavProps> = ({ page }) => {
 
   return (
     <div className="flex justify-center items-center w-full p-10">
-      <nav className="absolute rounded-2xl w-full mx-auto md:w-1/2 z-50 backdrop-blur-md bg-white/10 border-b border-white/20 shadow-md">
+      <nav className="absolute rounded-2xl w-full mx-auto md:w-3/5 z-50 backdrop-blur-md bg-white/10 border-b border-white/20 shadow-md">
         <div className="w-full px-4 py-3 flex justify-end items-center">
           {/* Desktop Menu */}
           <ul className="hidden md:flex gap-6 text-white font-regular justify-center w-full">
             <li><Link to="/" onClick={closeMenu} className={linkClass('home')}>Home</Link></li>
-            <li><Link to="/about" onClick={closeMenu} className={linkClass('about')}>About</Link></li>
-            <li><Link to="/services" onClick={closeMenu} className={linkClass('services')}>Services</Link></li>
-            <li><Link to="/portfolio" onClick={closeMenu} className={linkClass('portfolio')}>Portfolio</Link></li>
+            <li><Link to="/" onClick={closeMenu} className={linkClass('about')}>About</Link></li>
+            <li><Link to="/about" onClick={closeMenu} className={linkClass('about')}>Shopping</Link></li>
+            <li><Link to="/services" onClick={closeMenu} className={linkClass('services')}>Dining</Link></li>
+            <li><Link to="/portfolio" onClick={closeMenu} className={linkClass('portfolio')}>Emtertainment</Link></li>
+            <li><Link to="/contact" onClick={closeMenu} className={linkClass('contact')}>Wax Museum</Link></li>
             <li><Link to="/contact" onClick={closeMenu} className={linkClass('contact')}>Contact</Link></li>
           </ul>
 
@@ -51,11 +53,13 @@ const NavBar: React.FC<NavProps> = ({ page }) => {
         {menuOpen && (
           <div className="w-full md:hidden px-4 pb-4 pt-2 backdrop-blur-md bg-white/10 text-white">
             <ul className="flex flex-col gap-4 font-medium items-center">
-              <li><Link to="/" onClick={closeMenu} className={linkClass('home')}>Home</Link></li>
-              <li><Link to="/about" onClick={closeMenu} className={linkClass('about')}>About</Link></li>
-              <li><Link to="/services" onClick={closeMenu} className={linkClass('services')}>Services</Link></li>
-              <li><Link to="/portfolio" onClick={closeMenu} className={linkClass('portfolio')}>Portfolio</Link></li>
-              <li><Link to="/contact" onClick={closeMenu} className={linkClass('contact')}>Contact</Link></li>
+                <li><Link to="/" onClick={closeMenu} className={linkClass('home')}>Home</Link></li>
+                <li><Link to="/" onClick={closeMenu} className={linkClass('about')}>About</Link></li>
+                <li><Link to="/about" onClick={closeMenu} className={linkClass('about')}>Shopping</Link></li>
+                <li><Link to="/services" onClick={closeMenu} className={linkClass('services')}>Dining</Link></li>
+                <li><Link to="/portfolio" onClick={closeMenu} className={linkClass('portfolio')}>Emtertainment</Link></li>
+                <li><Link to="/contact" onClick={closeMenu} className={linkClass('contact')}>Wax Museum</Link></li>
+                <li><Link to="/contact" onClick={closeMenu} className={linkClass('contact')}>Contact</Link></li>
             </ul>
           </div>
         )}
