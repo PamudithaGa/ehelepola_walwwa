cc1c252e660222e59937f734088df7eda0d75d2c
Pamuditha Gangana <pamudithagangana45@gmail.com>
2025-07-10 22:49:43 +0530

    Update Prison Arcade

--- src/pages/PrisonArcade.tsx
+++ src/pages/PrisonArcade.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 //Components
 import Hero from "../components/prisonarcade/Hero";
@@ -6,16 +6,37 @@ import Choices from "../components/prisonarcade/Choices";
 import Places from "../components/prisonarcade/Places";
 import Gallery from "../components/prisonarcade/Gallery";
 import Footer from "../components/common/Footer";
+import NavBar from "../components/common/NavBar";
+import HeroSection from "../components/home/HeroSection";
 
-import FooterTopImage from '../assets/footer-image.png'
+import FooterTopImage from "../assets/footer-image.png";
 const PrisonArcade: React.FC = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowNav(true);
+    }, 2000); // Delay 5 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <section>
+      {/* header */}
+      <section
+        className={`absolute w-full z-50 transition-all duration-2000 ease-in-out ${
+          showNav ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"
+        }`}
+      >
+        <NavBar page="home" />
+      </section>
+
+      <section className="lg:h-[100dvh]">
         <Hero />
       </section>
 
-      <section className="h-[100dvh] flex items-center bg-amber-300">
+      <section className="lg:h-[100dvh] flex items-center justify-center">
         <Choices />
       </section>
 
@@ -28,13 +49,11 @@ const PrisonArcade: React.FC = () => {
       </section>
 
       {/* footer */}
-      <section
-        className="mt-10 ">
+      <section className="mt-10 ">
         <div>
           <img src={FooterTopImage} />
         </div>
-        <section
-          className="bg-primary z-10">
+        <section className="bg-primary z-10">
           <Footer />
         </section>
       </section>
