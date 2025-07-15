6e4cea844685227b375f56f82412783f3ead0da9
Pamuditha Gangana <pamudithagangana45@gmail.com>
2025-07-15 12:10:22 +0530

    Update responsive

--- src/components/contactus/Map.tsx
+++ src/components/contactus/Map.tsx
@@ -7,14 +7,14 @@ import Walawwa from '../../assets/logos/walawwa-building.png'
 const Map:React.FC = () => {
   return (
     <div className='relative overflow-hidden'>
-      <div className=" h-[auto] md:h-auto md:mb-30 mb-10 mx-auto flex flex-col justify-center items-center overflow-hidden">
-        <div className='w-1/2'>
-              <h2 className="text-6xl font-bold text-center textprimary md:pt-0 lg:pt-0">
+      <div className=" h-[auto] md:h-auto md:mb-30 lg:mb-10 mb-30 mx-auto flex flex-col justify-center items-center overflow-hidden">
+        <div className='lg:w-1/2'>
+              <h2 className="lg:text-6xl text-[56px] font-bold text-center  text-primary md:pt-0 lg:pt-0">
               Our Location
             </h2>
               <img src={Walawwa} alt="" />
             </div>
-          <div className="w-1/2 text-center">
+          <div className="lg:w-1/2 text-center">
             
             <p className="text-gray-400 font-normal text-base mt-8 mb-10 w-full leading-7">
               {" "}
