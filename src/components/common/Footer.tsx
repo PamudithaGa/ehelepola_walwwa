c61cb2f086bb124f69317f31fe53ac3b20ef9b33
Ashan Vimukthi <ashanwithanarachchi@gmail.com>
2025-07-24 13:38:52 +0530

    update footer nav links

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -45,7 +45,7 @@ const Footer: React.FC = () => {
             <ul className="flex flex-col gap-2 text-[16px]">
               <li>
                 <Link
-                  to=""
+                  to="/WaxMuseum"
                   className="hover:text-white transition-colors duration-300"
                 >
                   Wax Museum
@@ -53,7 +53,7 @@ const Footer: React.FC = () => {
               </li>
               <li>
                 <Link
-                  to=""
+                  to="/PrisonArcade"
                   className="hover:text-white transition-colors duration-300"
                 >
                   Prison Arcade
@@ -61,7 +61,7 @@ const Footer: React.FC = () => {
               </li>
               <li>
                 <Link
-                  to=""
+                  to="/kingscourt"
                   className="hover:text-white transition-colors duration-300"
                 >
                   Kings Court
@@ -69,7 +69,7 @@ const Footer: React.FC = () => {
               </li>
               <li>
                 <Link
-                  to=""
+                  to="/courtyard"
                   className="hover:text-white transition-colors duration-300"
                 >
                   Court Yard
