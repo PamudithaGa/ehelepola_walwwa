d1c46b66a3159420e17bc57c78fab6a9136d1878
Pamuditha Gangana <pamudithagangana45@gmail.com>
2025-07-25 09:21:32 +0530

    Prison Arcade Update

--- src/components/prisonarcade/Choices.tsx
+++ src/components/prisonarcade/Choices.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
 
+//Logos of choices
 import Ayurweda from "../../assets/logos/ayurveda.webp";
 import Brassware from "../../assets/logos/brassware.webp";
 import clay from "../../assets/logos/clay.webp";
@@ -21,137 +25,228 @@ import Photography from "../../assets/logos/photography.webp";
 import Batik from "../../assets/logos/batik.webp";
 import Leather from "../../assets/logos/leather.webp";
 
+//Logos of companies
+import DelmahLogo from "../../assets/prisonarcade/DilmahLogoColour.jpg";
+import SpaCelonLogo from "../../assets/prisonarcade/spacelonLOgo.jpg";
+
+//Promo Banners
+import spacelonPromo from "../../assets/prisonarcade/spacelonBannerHorizontal.png";
+import EarthboundCreationsPromo from "../../assets/prisonarcade/EarthboundCreationsBannerHorizontal.png";
+// import AnotherLogo from "../../assets/prisonarcade/dilmateaBanner.png";
+
+const promoImages = [spacelonPromo, EarthboundCreationsPromo];
+
 type Card = {
   id: number;
   title: string;
+  name: string;
   image: string;
   description: string;
+  backImage: string;
+  moreDetails: string;
 };
 
 const cardsData: Card[] = [
   {
     id: 1,
-    title: "Ayuruweda",
-    image: Ayurweda,
-    description: "Luxury accessories once safeguarded in the vaults.",
+    title: "Cosmetics",
+    name: "Spa Ceylon",
+    image: cosmetics,
+    description: "Worn by guards of the royal royal prison arcade.",
+    backImage: SpaCelonLogo,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
+
   {
     id: 2,
-    title: "Brassware",
-    image: Brassware,
-    description: "Symbol of historic access to secret chambers.",
+    title: "Tea",
+    name: "Dilmah",
+    image: Tea,
+    description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: DelmahLogo,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
+
   {
     id: 3,
     title: "Clay",
+    name: "Spa Ceylon",
     image: clay,
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 4,
-    title: "Cosmetics",
-    image: cosmetics,
-    description: "Worn by guards of the royal royal prison arcade.",
+    title: "Ayuruweda",
+    name: "Spa Ceylon",
+    image: Ayurweda,
+    description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: SpaCelonLogo,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 5,
     title: "Gems",
+    name: "Spa Ceylon",
+
     image: Gems,
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 6,
     title: "Handloom",
+    name: "Spa Ceylon",
+
     image: Handloom,
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 7,
     title: "Jewellery",
     image: jewellery,
+    name: "Spa Ceylon",
+
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 8,
     title: "Laksha",
     image: Laksha,
+    name: "Spa Ceylon",
+
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 9,
     title: "Mask",
+    name: "Spa Ceylon",
+
     image: Mask,
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 10,
     title: "Porcelain",
+    name: "Spa Ceylon",
+
     image: Porcelain,
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 11,
     title: "Spices",
+    name: "Spa Ceylon",
+
     image: Spices,
     description: "Inspired by candle-lit hhfgfhj corridors of the past.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 12,
     title: "Silver",
+    name: "Spa Ceylon",
+
     image: Silver,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 13,
     title: "Stoneware",
+    name: "Spa Ceylon",
+
     image: Stoneware,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 14,
-    title: "Tea",
-    image: Tea,
-    description: "Luxury accessories once safeguarded in the vaults.",
+    title: "Brassware",
+    name: "Spa Ceylon",
+
+    image: Brassware,
+    description: "Symbol of historic access to secret chambers.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 15,
     title: "Paintings",
+    name: "Spa Ceylon",
     image: Paintings,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 16,
     title: "Papers",
+    name: "Spa Ceylon",
+
     image: Paper,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 17,
     title: "Wood Carving",
+    name: "Spa Ceylon",
+
     image: Woodcarving,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 18,
     title: "Photography",
+    name: "Spa Ceylon",
+
     image: Photography,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 19,
     title: "Batik",
+    name: "Spa Ceylon",
+
     image: Batik,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
   {
     id: 20,
     title: "Leather",
+    name: "Spa Ceylon",
+
     image: Leather,
     description: "Luxury accessories once safeguarded in the vaults.",
+    backImage: Brassware,
+    moreDetails: "Luxury accessories once safeguarded in the vaults.",
   },
 ];
 
-const CARDS_PER_PAGE = 12;
+const CARDS_PER_PAGE = 9;
 
 const Choices: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -174,15 +269,41 @@ const Choices: React.FC = () => {
   }
 
   return (
-    <section className="text-white px-4 min-h-screen overflow-x-auto">
-      <div className="max-w-7xl mx-auto space-y-10">
+    <section className="text-white px-4 lg:min-h-screen overflow-x-auto ">
+      <div
+        className="max-w-7xl mx-auto space-y-10 
+      "
+      >
         {/* Cards */}
         {!isMobile ? (
           // Desktop Grid
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:gap-6">
-            {currentCards.map((card) => (
-              <CardItem key={card.id} card={card} />
-            ))}
+          <div className="flex gap-2">
+            <div className="w-1/4  rounded-2xl overflow-hidden">
+              <Swiper
+                modules={[Autoplay]}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                loop
+                speed={800}
+                slidesPerView={1}
+                className="rounded-2xl"
+              >
+                {promoImages.map((img, index) => (
+                  <SwiperSlide key={index}>
+                    <img
+                      src={img}
+                      alt={`Promo ${index + 1}`}
+                      className="w-full h-auto rounded-2xl object-contain pr-1"
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+
+            <div className="w-3/4 grid grid-cols-1 lg:grid-cols-3  lg:gap-3">
+              {currentCards.map((card) => (
+                <CardItem key={card.id} card={card} />
+              ))}
+            </div>
           </div>
         ) : (
           <div
@@ -199,6 +320,28 @@ const Choices: React.FC = () => {
                 ))}
               </div>
             ))}
+            <div>
+              <div className="w-1/4  rounded-2xl overflow-hidden">
+                <Swiper
+                  modules={[Autoplay]}
+                  autoplay={{ delay: 3000, disableOnInteraction: false }}
+                  loop
+                  speed={800}
+                  slidesPerView={1}
+                  className="rounded-2xl"
+                >
+                  {promoImages.map((img, index) => (
+                    <SwiperSlide key={index}>
+                      <img
+                        src={img}
+                        alt={`Promo ${index + 1}`}
+                        className="w-full h-auto rounded-2xl object-contain pr-1"
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+            </div>
           </div>
         )}
 
@@ -237,27 +380,51 @@ const Choices: React.FC = () => {
           </div>
         )}
       </div>
+      <div></div>
     </section>
   );
 };
 
 const CardItem: React.FC<{ card: Card }> = ({ card }) => (
-  <div className="bg-gradient-to-tr from-black via-[#0c0c0c]  to-black lg:w-[100%] w-[90%] lg:py-8 py-6 px-8 rounded-lg overflow-x-auto shadow-md relative">
-    <div className="h-3/6 ">
-      <div className="w-15 h-15 rounded-full bg-white/30 backdrop-blur-md border border-white/30 shadow-lg flex items-center justify-center ">
-        <img
-          src={card.image}
-          alt={card.title}
-          className="w-10 h-10 object-contain"
-        />
+  <div className="flip-card-container w-[90%] lg:w-[100%]">
+    <div className="flip-card">
+      {/* Front Side */}
+      <div className="flip-card-front bg-gradient-to-tr from-black via-[#0c0c0c] to-black lg:py-8 py-6 px-8 rounded-lg overflow-x-auto shadow-md">
+        <div className="h-3/6">
+          <div className="w-15 h-15 rounded-full bg-white/30 backdrop-blur-md border border-white/30 shadow-lg flex items-center justify-center">
+            <img
+              src={card.image}
+              alt={card.title}
+              className="w-10 h-10 object-contain"
+            />
+          </div>
+        </div>
+        <div className="h-1/6 pt-2 lg:pt-1">
+          <p className="text-white/70 text-base">{card.title}</p>
+        </div>
+        <div className="h-2/6 pt-3 lg:pt-1">
+          <p className="text-white/30 text-sm">{card.description}</p>
+        </div>
       </div>
-    </div>
-    <div className="h-1/6 pt-2 lg:pt-1">
-      <p className="text-white/70  text-base">{card.title}</p>
-    </div>
-    <div className="h-2/6 pt-3 lg:pt-1">
-      <p className="text-white/30 text-sm">{card.description}</p>
+
+      {/* Back Side */}
+      <div className="flip-card-back bg-gradient-to-tr from-[#1a1a1a] via-[#111] to-[#000] lg:py-8 py-6 px-8 rounded-lg overflow-x-auto shadow-md text-white">
+        <div className="w-15 h-15 rounded-full bg-white/30 backdrop-blur-md border border-white/30 shadow-lg flex items-center justify-center">
+          <img
+            src={card.backImage}
+            alt={card.title}
+            className="w-full h-full object-contain rounded-full"
+          />
+        </div>
+        <div className="h-1/6 pt-2 lg:pt-1">
+          <p className="text-white/70 text-base">{card.name}</p>
+        </div>
+        <div className="h-2/6 pt-3 lg:pt-1">
+          <p className="text-white/30 text-sm">{card.moreDetails}</p>
+        </div>
+      </div>
     </div>
   </div>
 );
+
 export default Choices;
